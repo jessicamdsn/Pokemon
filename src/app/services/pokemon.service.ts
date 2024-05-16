788c58b0d92e3059e384be1bafe7c8eac34612dc
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2024-05-16 13:09:07 -0300

    trabalhando com modulo http

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
   private baseURL:string =""
+  private pokeData:any
 
-  constructor() { 
+  constructor(private http:HttpClient) { 
     this.baseURL = environment.pokeApi
   }
 
   getPokemon(pokemonName:string){
-    console.log(this.baseURL)
+    this.pokeData = this.http.get(`${this.baseURL}${pokemonName}`)
   }
 
 }
