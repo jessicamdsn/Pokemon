c7098b8f661e6bf11ae1de351d5c8e3c9ad01498
Jessica neves <128631606+jessicamdsn@users.noreply.github.com>
2024-05-17 00:23:48 -0300

    Armazena o Pokémon atual

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -11,37 +11,45 @@ import { PokemonData } from '../../models/pokemonData';
   styleUrl: './card.component.css'
 })
 export class CardComponent implements OnInit {
-  pokemon:PokemonData 
+  pokemon: PokemonData;
+  defaultPokemon: PokemonData;
 
-  
   constructor(
-    private service:PokemonService
-  ){
+    private service: PokemonService
+  ) {
     this.pokemon = {
-      id:0, name:'',
-      sprites:{
+      id: 0,
+      name: '',
+      sprites: {
         front_default: ''
-      },types:[]
+      },
+      types: []
+    };
+
+    this.defaultPokemon = { ...this.pokemon }; // Inicializa com um valor padrão
+  }
+
+  ngOnInit(): void {
+    this.getPokemon('pikachu');
+  }
+
+  getPokemon(searchName: string) {
+    if (searchName.trim() === '') {
+      // Se o campo de busca estiver vazio, restaura o Pokémon padrão
+      this.pokemon = { ...this.defaultPokemon };
+    } else {
+      this.service.getPokemon(searchName).subscribe({
+        next: (res) => {
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          };
+          this.defaultPokemon = { ...this.pokemon }; // Atualiza o Pokémon padrão
+        },
+        error: (err) => console.log('not found')
+      });
     }
   }
-  
-  ngOnInit(): void {
-    this.getPokemon("pikachu")
-  }
-
-  getPokemon(searchName:string){
-    this.service.getPokemon(searchName).subscribe({
-      next: (res) => {console.log(res)
-
-        this.pokemon = {
-          id: res.id,
-          name: res.name,
-          sprites: res.sprites,
-          types: res.types
-        }
-      },
-      error: (err) => console.log('not found')
-    })
-  }
-
 }
